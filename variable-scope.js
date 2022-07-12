fb0a046f552c55fab1904b8c5b77e89043bef8bc
Vinay Choudhary <vinaychoudhary994@gmail.com>
2022-07-12 16:57:18 +0530

    Update variable-scope.js

--- variable-scope.js
+++ variable-scope.js
@@ -559,7 +559,17 @@ let x=10, y=10;
   console.log(newArr.push(34));
   console.log(newArr);
 
+//--------------------------------------------------------
+ //MAP
+ const articlePrices= [10.99,5.99,3.99,6.59];
 
+ const tax=1.2;
+
+ const totalPrices=articlePrices.map((value, idx, articlePrices) => {
+  const artvalue= {index:idx, totalPrice: value + tax };
+  return artvalue;
+});
+console.log(articlePrices, totalPrices);
 
       
       
